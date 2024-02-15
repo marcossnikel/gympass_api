9cc7dfd48c4c43d1063c250ce18df5c53937a48e
marcossnikel <marcosnikeldev@gmail.com>
2024-02-15 18:40:37 -0300

    feat: Add in-memory check in repo

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,19 @@
+import { CheckIn, Prisma } from '@prisma/client'
+import { CheckInsRepository } from '../check-ins-repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  items: CheckIn[] = []
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      created_at: new Date(),
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+    }
+    this.items.push(checkIn)
+
+    return checkIn
+  }
+}
