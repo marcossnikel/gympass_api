928be02c36d7d845c973b70d6f839c858c23bd48
marcossnikel <marcosnikeldev@gmail.com>
2024-02-15 18:40:59 -0300

    feat: New `findById`method into fake repo and use randomUUID

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,11 +1,12 @@
 import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
+import { randomUUID } from 'crypto'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: '01',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
@@ -19,6 +20,14 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async findByEmail(email: string) {
     const user = this.items.find((item) => item.email === email)
+
+    if (!user) return null
+
+    return user
+  }
+
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id)
 
     if (!user) return null
 
