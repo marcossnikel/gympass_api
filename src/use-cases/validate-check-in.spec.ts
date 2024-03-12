1f0647c479f110c4b9b8b233f7fef45667631715
marcossnikel <marcosnikeldev@gmail.com>
2024-03-11 21:46:26 -0300

    feat: add validate check in use case

---
+++ src/use-cases/validate-check-in.spec.ts
@@ -0,0 +1,60 @@
+import { describe, expect, it, beforeEach, vi, afterEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { ValidateCheckInUseCase } from './validate-check-in'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: ValidateCheckInUseCase
+
+describe('Validate check in use case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new ValidateCheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to validate check in', async () => {
+    // vi.setSystemTime(new Date('2024-01-01 10:00:00'))
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    })
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it('it should be not able to validate inexistent check in', async () => {
+    await expect(() =>
+      sut.execute({
+        checkInId: 'inexistent id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('it should not be able to validate the check in after 20 minuts of its creation', async () => {
+    vi.setSystemTime(new Date(2024, 0, 1, 13, 40))
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+    const twentyOneMinutsInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutsInMs)
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError)
+  })
+})
