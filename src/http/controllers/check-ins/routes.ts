a290a373815bfd163b073d012c198ee8c4a07345
marcossnikel <marcosnikeldev@gmail.com>
2024-03-13 23:09:55 -0300

    feat: pass role verification middleware

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,6 +4,7 @@ import { create } from './create'
 import { history } from './history'
 import { validate } from './validate'
 import { metrics } from './metrics'
+import { verifyUserRole } from '../middlewares/verify-user-role'
 
 export async function checkInRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
@@ -11,5 +12,9 @@ export async function checkInRoutes(app: FastifyInstance) {
   app.post('/gyms:gymId/check-ins', create)
   app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
-  app.patch('/check-ins/:checkInId/validate', validate)
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 }
