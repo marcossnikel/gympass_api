7f47fdefa832a7ebda5554676c0cab345124dde3
marcossnikel <marcosnikeldev@gmail.com>
2024-03-13 22:35:18 -0300

    feat: add check-in routes

---
+++ src/http/controllers/check-ins/routes.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from 'fastify'
+import { verifyJwt } from '../middlewares/verify-jwt'
+import { create } from './create'
+import { history } from './history'
+import { validate } from './validate'
+import { metrics } from './metrics'
+
+export async function checkInRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJwt)
+
+  app.post('/gyms:gymId/check-ins', create)
+  app.get('/check-ins/history', history)
+  app.get('/check-ins/metrics', metrics)
+  app.patch('/check-ins/:checkInId/validate', validate)
+}
