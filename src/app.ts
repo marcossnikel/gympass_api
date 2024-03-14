2acf9889510ff3285236e8c5fe08b1f460dac340
marcossnikel <marcosnikeldev@gmail.com>
2024-03-13 22:34:41 -0300

    feat: add cookie handling and new routes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,27 @@
 import { fastify } from 'fastify'
-import { appRoutes } from './http/routes'
+import fastifyCookie from '@fastify/cookie'
+import { userRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import { gymsRoutes } from './http/controllers/gyms/routes'
+import { checkInRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: { expiresIn: '10m' },
 })
-app.register(appRoutes)
+
+app.register(userRoutes)
+app.register(gymsRoutes)
+app.register(checkInRoutes)
+app.register(fastifyCookie)
+
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
